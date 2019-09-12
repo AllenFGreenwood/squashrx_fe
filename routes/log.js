fd0bb82ec43c88aa20f16b8b03b3ca1836fdb0e2
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 15:36:39 -0700

    add tostring

--- routes/log.js
+++ routes/log.js
@@ -21,9 +21,9 @@ router.get('/log', function (req, res, next) {
         log.find().sort({ "log_created": -1 }).limit(10).toArray((err, recent) => {
             for (index = 0; index < recent.length; index++) {
                 let startTime = moment(recent[index]['ab_res_start_time']).tz('America/New_York');
-                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss');
+                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss').toString();
                 let endTime = moment(recent[index]['ab_res_end_time']).tz('America/New_York');
-                recent[index]['ab_res_end_time'] = endTime.format('YYYY-MM-DD HH:mm:ss');
+                recent[index]['ab_res_end_time'] = endTime.format('YYYY-MM-DD HH:mm:ss').toString();
             }
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
