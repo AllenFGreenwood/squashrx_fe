992097cf558f5dfd712c71708457c976919c9b42
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 15:34:10 -0700

    format start and end times

--- routes/log.js
+++ routes/log.js
@@ -21,7 +21,9 @@ router.get('/log', function (req, res, next) {
         log.find().sort({ "log_created": -1 }).limit(10).toArray((err, recent) => {
             for (index = 0; index < recent.length; index++) {
                 let startTime = moment(recent[index]['ab_res_start_time']).tz('America/New_York');
-                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss').toLocaleLowerCase();
+                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss');
+                let endTime = moment(recent[index]['ab_res_end_time']).tz('America/New_York');
+                recent[index]['ab_res_end_time'] = endTime.format('YYYY-MM-DD HH:mm:ss');
             }
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
