a875aa5864dbd4d91c623d43dde626e815eaad6d
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 14:58:18 -0700

    8

--- routes/log.js
+++ routes/log.js
@@ -21,7 +21,7 @@ router.get('/log', function (req, res, next) {
         log.find().sort({ "log_created": -1 }).limit(10).toArray((err, recent) => {
             for (index = 0; index < recent.length; index++) {
                 let startTime = moment(recent[index]['ab_res_start_time']).tz('America/New_York');
-                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss');
+                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss').toLocaleLowerCase();
             }
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
