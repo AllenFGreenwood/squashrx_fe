4346859000ebbe54933475bf5c342195d5c46d2b
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 13:23:46 -0700

    change last scan to nyc time

--- routes/log.js
+++ routes/log.js
@@ -21,8 +21,8 @@ router.get('/log', function (req, res, next) {
         log.find().sort({ "log_created": -1 }).limit(10).toArray((err, recent) => {
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
-                let lastTime = new Date(last[0]['ab_creation_time_window_from']);
-                let fifteen_min_early_nyc = new Date(moment(last[0]['ab_creation_time_window_from']).tz('America/New_York'));
+                //let lastTime = new Date(last[0]['ab_creation_time_window_from']);
+                let lastTime = new Date(moment(last[0]['ab_creation_time_window_from']).tz('America/New_York'));
 
                 //lastTime = fifteen_min_early_nyc.toISOString();
                 let lastDb = last[0]['ab_creation_time_window_from'];
