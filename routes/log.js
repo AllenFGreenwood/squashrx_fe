be218508cefa8680940ac0f2657a02f00192f2d7
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 15:39:11 -0700

    x

--- routes/log.js
+++ routes/log.js
@@ -20,8 +20,8 @@ router.get('/log', function (req, res, next) {
 
         log.find().sort({ "log_created": -1 }).limit(10).toArray((err, recent) => {
             for (index = 0; index < recent.length; index++) {
-                let startTime = moment(recent[index]['ab_res_start_time']).tz('America/New_York');
-                recent[index]['ab_res_start_time'] = startTime.format('YYYY-MM-DD HH:mm:ss').toString();
+                let startTime = new Date(moment(recent[index]['ab_res_start_time']).tz('America/New_York'));
+                recent[index]['ab_res_start_time'] = startTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
                 let endTime = moment(recent[index]['ab_res_end_time']).tz('America/New_York');
                 recent[index]['ab_res_end_time'] = endTime.format('YYYY-MM-DD HH:mm:ss').toString();
             }
