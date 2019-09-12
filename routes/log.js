4b5e36fc26d3c0162d43fab428bd9e610e6d4270
Allen Greenwood <allengreenwood@gmail.com>
2019-09-12 15:58:58 -0700

    c

--- routes/log.js
+++ routes/log.js
@@ -24,8 +24,8 @@ router.get('/log', function (req, res, next) {
                 recent[index]['ab_res_start_time'] = startTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
                 let endTime = new Date(moment(recent[index]['ab_res_end_time']).tz('America/New_York'));
                 recent[index]['ab_res_end_time'] = endTime;//.format('YYYY-MM-DD HH:mm:ss').toString();                
-                let endTime = new Date(moment(recent[index]['ab_res_creation_time']).tz('America/New_York'));
-                recent[index]['ab_res_creation_time'] = endTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
+                let madeTime = new Date(moment(recent[index]['ab_res_creation_time']).tz('America/New_York'));
+                recent[index]['ab_res_creation_time'] = madeTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
             }
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
