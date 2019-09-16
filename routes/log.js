2827837fcc0fa06f2f5c0a8a3d04c2bf8213b421
Allen Greenwood <allengreenwood@gmail.com>
2019-09-16 13:15:26 -0700

    n

--- routes/log.js
+++ routes/log.js
@@ -27,35 +27,20 @@ router.get('/log', function (req, res, next) {
                 //recent[index]['ab_res_start_time'] = startTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
                 recent[index]['abresstarttime'] = parsed.toLocaleString("en-US", { timeZone: "America/New_York" });
                 //recent[index]['abresstarttime'] = recent[index]['abresstarttime'].replace(/+0000 (Greenwich Mean Time)/, '');
-                //let endTime = new Date(moment(recent[index]['ab_res_end_time']).tz('America/New_York'));
-                milli = Date.parse(recent[index]['ab_res_end_time']);
-                parsed = new Date(milli);
-                recent[index]['abresendtime'] = parsed.toLocaleString("en-US", { timeZone: "America/New_York" });//.format('YYYY-MM-DD HH:mm:ss').toString();  
-
-
-
-                //recent[index]['abresendtime'] = endTime; //.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+                let endTime = new Date(moment(recent[index]['ab_res_end_time']).tz('America/New_York'));
+                recent[index]['ab_res_end_time'] = endTime;//.format('YYYY-MM-DD HH:mm:ss').toString();   
+                recent[index]['abresendtime'] = endTime; //.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
                 //recent[index]['abresendtime'] = recent[index]['abresendtime'].replace(/+0000 (Greenwich Mean Time)/, '');
-                //let madeTime = new Date(moment(recent[index]['ab_res_creation_time']).tz('America/New_York'));
-
-                milli = Date.parse(recent[index]['ab_res_creation_time']);
-                parsed = new Date(milli);
-
-
-                //recent[index]['ab_res_creation_time'] = madeTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
-                recent[index]['abrescreationttime'] = parsed.toLocaleString("en-US", { timeZone: "America/New_York" }); //.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+                let madeTime = new Date(moment(recent[index]['ab_res_creation_time']).tz('America/New_York'));
+                recent[index]['ab_res_creation_time'] = madeTime;//.format('YYYY-MM-DD HH:mm:ss').toString();
+                recent[index]['abrescreationttime'] = madeTime; //.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
                 //recent[index]['abrescreationttime'] = 
             }
             var lastScan = db.collection('LastScan');
             lastScan.find().limit(1).toArray((err, last) => {
                 //let lastTime = new Date(last[0]['ab_creation_time_window_from']);
-                milli = Date.parse(recent[index]['ab_creation_time_window_from']);
-                parsed = new Date(milli);
-
-                let lastTime = parsed.toLocaleString("en-US", { timeZone: "America/New_York" }); //.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-
-                //let lastTime = new Date(moment(last[0]['ab_creation_time_window_from']).tz('America/New_York')).toString();
-                //lastTime = lastTime.toString();
+                let lastTime = new Date(moment(last[0]['ab_creation_time_window_from']).tz('America/New_York')).toString();
+                lastTime = lastTime.toString();
                 //lastTime = lastTime.replace(/+0000 (Greenwich Mean Time)/, '');
                 //lastTime = lastTime.format('YYYY-MM-DD HH:mm:ss');
 
